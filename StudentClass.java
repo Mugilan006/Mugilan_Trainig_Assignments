cc12008bf897954c2d72327d5f8b4f4122777b95
Mugilan006 <mugijayabalan@gmail.com>
2025-12-03 17:34:58 +0530

    First commit

---
+++ StudentClass.java
@@ -0,0 +1,30 @@
+class Student {
+    private String name;
+    private int rollNo;
+    private int marks;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+    public void setMarks(int marks) {
+        if (marks < 0 || marks > 100) {
+            System.out.println("Invalid marks! Must be between 0 and 100.");
+        } else {
+            this.marks = marks;
+        }
+    }
+}
